66bfc81141032db5bfb4f9f51643a2b7294a1722
Khoi Nguyen Tan <khoi.nt.21@gmail.com>
2020-09-18 08:08:33 +0700

    version 1.0.0 - Complete Taskbar

---
+++ components/TaskbarInfo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-dark collapse" id="navbarHeader">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 col-md-7 py-4">
+          <h4 class="text-white">Album</h4>
+          <p class="text-muted">"ASUS will bring you to the next revolution"</p>
+        </div>
+        <div class="col-sm-4 offset-md-1 py-4">
+          <h4 class="text-white">Contact</h4>
+          <ul class="list-unstyled">
+            <li>
+              <a href="#" class="text-white">Follow in Twitter</a>
+            </li>
+            <li>
+              <a href="#" class="text-white">Follow in Facebook</a>
+            </li>
+            <li>
+              <a href="#" class="text-white">Follow in Instagram</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+/* div {
+  width: 100px;
+  height: 100px;
+  border: 1px solid orangered;
+} */
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="header">
+    <TaskbarInfo />
+    <Taskbar />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+header {
+  display: none;
+}
+</style>
